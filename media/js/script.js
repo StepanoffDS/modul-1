f00451a3b0cd39a2729aaf44af6ea28b092a63b7
Dmitrii <stepanov.dmitrii.spo@gmail.com>
2024-12-11 16:34:44 +0400

    test vk video 5

--- media/js/script.js
+++ media/js/script.js
@@ -4,38 +4,28 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	function playVideo(iframe) {
 		return new Promise((resolve) => {
-			// Получаем URL для подгрузки видео
 			let videoSrc = iframe.src;
 
-			// Добавляем параметр autoplay=1
 			videoSrc += "&autoplay=1";
 
 			// Создаем контейнер для превью
 			const previewContainer = document.createElement("div");
 			previewContainer.className = "videoplayer_thumb";
-
-			// Получаем изображение записи WordPress (предполагается, что это изображение на странице)
-			const featuredImage = document.querySelector('meta[property="og:image"]');
-			const previewImageUrl = featuredImage ? featuredImage.content : ""; // Получаем URL изображения
-
-			// Устанавливаем изображение записи как фон для превью, если оно существует
+			const previewImageUrl = iframe.getAttribute("data-logo-src");
 			if (previewImageUrl) {
 				previewContainer.style.backgroundImage = `url(${previewImageUrl})`;
 			} else {
-				// Устанавливаем заглушку, если изображение не найдено
-				previewContainer.style.backgroundImage = `url('https://tiku.ru/wp-content/themes/Tiku/img/tiku-pattern.webp')`; // Укажите путь к вашей заглушке
+				previewContainer.style.backgroundImage = `url('https://tiku.ru/wp-content/themes/Tiku/img/tiku-pattern.webp')`;
 			}
 
 			// Создаем контейнер для иконки плей
 			const playIconContainer = document.createElement("div");
 			playIconContainer.className = "play-icon-container";
-
-			// Вставляем вашу SVG-иконку
 			const playIcon = document.createElement("div");
 			playIcon.className = "play-icon";
 			playIcon.innerHTML = `
-		<svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" fill="#ffffff" viewBox="0 0 256 256"><path d="M240,128a15.74,15.74,0,0,1-7.6,13.51L88.32,229.65a16,16,0,0,1-16.2.3A15.86,15.86,0,0,1,64,216.13V39.87a15.86,15.86,0,0,1,8.12-13.82,16,16,0,0,1,16.2.3L232.4,114.49A15.74,15.74,0,0,1,240,128Z"></path></svg>
-	`;
+				<svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" fill="#ffffff" viewBox="0 0 256 256"><path d="M240,128a15.74,15.74,0,0,1-7.6,13.51L88.32,229.65a16,16,0,0,1-16.2.3A15.86,15.86,0,0,1,64,216.13V39.87a15.86,15.86,0,0,1,8.12-13.82,16,16,0,0,1,16.2.3L232.4,114.49A15.74,15.74,0,0,1,240,128Z"></path></svg>
+			`;
 
 			// Добавляем иконку в контейнер
 			playIconContainer.appendChild(playIcon);
@@ -65,5 +55,4 @@ document.addEventListener("DOMContentLoaded", function () {
 			console.log("All videos are ready to play");
 		},
 	);
-	// iframes.forEach((iframe) => playVideo(iframe));
 });
